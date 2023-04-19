b0f3dcc0b09c8cc0e55934315ce768d2980d2480
Koudous Salifou <koudous@brumaire.io>
2023-04-19 11:43:12 +0200

    fix: fix code style error

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,4 +2,5 @@ module.exports = {
   processors: [["@mapbox/stylelint-processor-arbitrary-tags"]],
   extends: ["stylelint-config-standard", "stylelint-config-clean-order"],
   rules: {},
+  ignoreFiles: ["public/index.html"],
 };
